7de0f2a827efec0e9306cc2efaca464a679d238a
hgg111 <2015935865@qq.com>
2019-07-27 08:32:32 +0800

    优化代码

--- text/t1.c
+++ text/t1.c
@@ -17,10 +17,12 @@
 #include<stdio.h>
 int main()
 {
-    int a;
-    int b;
+    int a = 0;
+    int b = 0;
     while(~scanf("%d%d",&a,&b))
+    {
         printf("%d\n",a+b);
+    }
     return 0;
 }
 
