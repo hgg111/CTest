778a0e1db518147fbd1b071ba3d247abd1c30d6b
hgg111 <2015935865@qq.com>
2019-08-02 19:03:05 +0800

    第31题代码

---
+++ text/t31.c
@@ -0,0 +1,35 @@
+/*
+题目描述
+写一函数，使输入的一个字符串按反序存放，在主函数中输入输出反序后的字符串（不包含空格）。
+输入
+一行字符
+输出
+逆序后的字符串
+样例输入
+123456abcdef
+样例输出
+fedcba654321
+*/
+#include<stdio.h>
+#include<string.h>
+void change(char a[])
+{
+    int i = 0;
+    int j = 0;
+    char t;
+    j = strlen(a);
+    for(i = 0;i < j/2;i++)
+    {
+        t = a[i];
+        a[i] = a[j-1-i];
+        a[j-1-i] = t;
+    }
+}
+int main31()
+{
+    char a[50];
+    scanf("%s",a);
+    change(a);
+    puts(a);
+    return 0;
+}
