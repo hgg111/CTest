ac816741905a3da24b5bb990c28bf99dea53dd44
hgg111 <2015935865@qq.com>
2019-08-19 11:26:58 +0800

    输入一个正整数n (1≤ n ≤6),再输入一个n 行n列的矩阵，找出该矩阵中绝对值最大的元素以及它的行下标和列下标

---
+++ text/t60.c
@@ -0,0 +1,50 @@
+/*
+题目描述
+输入一个正整数n (1≤ n ≤6),再输入一个n 行n列的矩阵，找出该矩阵中绝对值最大的元素以及它的行下标和列下标。
+
+输入
+n
+
+nxn
+
+输出
+数 行 列
+
+样例输入
+2
+1 2
+3 4
+样例输出
+4 2 2
+*/
+#include<stdio.h>
+
+int main60()
+{
+    int number = 0;
+    int i = 0;
+    int j = 0;
+    int max = 0;
+    int maxi = 0;
+    int maxj = 0;
+    int data[6][6] = {0};
+
+    scanf("%d",&number);
+    for(i = 1;i <= number;i++)
+    {
+        for(j = 1;j <= number;j++)
+        {
+            scanf("%d",&data[i][j]);
+            if(data[i][j] > max)
+            {
+                max = data[i][j];
+                maxi = i;
+                maxj = j;
+            }
+        }
+    }
+
+    printf("%d %d %d\n",max,maxi,maxj);
+
+    return 0;
+}
